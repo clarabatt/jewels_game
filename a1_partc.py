489455ae111161d6861c538c7c419f96116adfac
Clara Battesini <clarabattesini@gmail.com>
2023-12-05 06:18:04 -0500

    getting code from A1

--- a1_partc.py
+++ a1_partc.py
@@ -1,90 +1,155 @@
 # Copy over your a1_partc.py file here
-#    Main Author(s): 
+#    Main Author(s):
 #    Main Reviewer(s):
 
 
-
 class Stack:
+    def __init__(self, cap=10):
+        self.stack = [None] * cap
+        self.cap = cap
+        self.size = 0
 
-	def __init__(self, cap):
-		pass
+    def capacity(self):
+        return self.cap
 
-	def capacity(self):
-		pass
+    def push(self, data):
+        if self.size >= self.cap:
+            self.cap = self.cap * 2
+            newStack = [None] * self.cap
+            for i in range(self.size):
+                newStack[i] = self.stack[i]
+            self.stack = newStack
+        self.stack[self.size] = data
+        self.size += 1
 
-	def push(self, data):
-		pass
+    def pop(self):
+        if self.size == 0:
+            raise IndexError("pop() used on empty stack")
+        poppedVal = self.stack[self.size - 1]
+        self.stack[self.size - 1] = None
+        self.size -= 1
+        return poppedVal
 
-	def pop(self):
-		pass
+    def get_top(self):
+        if self.is_empty():
+            return None
+        return self.stack[self.size - 1]
 
-	def get_top(self):
-		pass
+    def is_empty(self):
+        return self.size == 0
 
-	def is_empty(self):
-		pass
-
-	def __len__(self):
-		pass
+    def __len__(self):
+        return self.size
 
 
 class Queue:
+    def __init__(self, cap=10):
+        self.cap = cap
+        self.size = 0
+        self.queue = [None] * cap
 
+    def capacity(self):
+        return self.cap
 
-	def __init__(self, cap):
-		pass
+    def enqueue(self, data):
+        if self.size >= self.cap:
+            self.cap *= 2
+        newQueue = [None] * self.cap
+        for i in range(self.size):
+            newQueue[i] = self.queue[i]
+        self.queue = newQueue
+        self.queue[self.size] = data
+        self.size += 1
 
-	def capacity(self):
-		pass
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("dequeue() used on empty queue")
+        item = self.queue[0]
+        for i in range(self.size - 1):
+            self.queue[i] = self.queue[i + 1]
+        self.queue[self.size - 1] = None
+        self.size -= 1
+        return item
 
-	def enqueue(self, data):
-		pass
+    def get_front(self):
+        if self.is_empty():
+            return None
+        return self.queue[0]
 
-	def dequeue(self):
-		pass
+    def is_empty(self):
+        return self.size == 0
 
-	def get_front(self):
-		pass
-
-	def is_empty(self):
-		pass
-
-	def __len__(self):
-		pass
-
+    def __len__(self):
+        return self.size
 
 
 class Deque:
+    def __init__(self, cap=10):
+        self.cap = cap
+        self.queue = [None] * cap
+        self.size = 0
 
-	def __init__(self, cap):
-		pass
+    def capacity(self):
+        return self.cap
 
-	def capacity(self):
-		pass
+    def increase_capacity(self):
+        if self.size >= self.cap:
+            self.cap *= 2
 
-	def push_front(self, data):
-		pass
+    def push_front(self, data):
+        self.increase_capacity()
+        newdequeu = [None] * self.cap
+        newdequeu[0] = data
+        for i in range(self.size):
+            newdequeu[i + 1] = self.queue[i]
+        self.queue = newdequeu
+        self.size += 1
 
-	def push_back(self, data):
-		pass
+    def push_back(self, data):
+        old_cap = self.cap
+        self.increase_capacity()
+        newdequeu = [None] * self.cap
+        for i in range(self.size):
+            newdequeu[i] = self.queue[i]
+        newdequeu[self.size] = data
+        self.queue = newdequeu
+        self.size += 1
 
-	def pop_front(self):
-		pass
+    def pop_front(self):
+        if self.is_empty():
+            raise IndexError("pop_front() used on empty deque")
+        item = self.queue[0]
+        for i in range(1, self.size):
+            self.queue[i - 1] = self.queue[i]
+        self.size -= 1
+        self.queue[self.size] = None
+        return item
 
-	def pop_back(self):
-		pass
+    def pop_back(self):
+        if self.is_empty():
+            raise IndexError("pop_back() used on empty deque")
+        self.size -= 1
+        item = self.queue[self.size]
+        self.queue[self.size] = None
+        return item
 
-	def get_front(self):
-		pass
+    def get_front(self):
+        if self.is_empty():
+            return None
+        return self.queue[0]
 
-	def get_back(self):
-		pass
+    def get_back(self):
+        if self.is_empty():
+            return None
+        return self.queue[self.size - 1]
 
-	def is_empty(self):
-		pass
+    def is_empty(self):
+        return self.size == 0
 
-	def __len__(self):
-		pass
+    def __len__(self):
+        return self.size
 
-	def __getitem__(self, k):
-		pass
+    def __getitem__(self, k):
+        if k >= self.size:
+            raise IndexError("Index out of range")
+        return self.queue[k]
--- a1_partd.py
+++ a1_partd.py
@@ -1,13 +1,68 @@
 # copy over your a1_partd.py file here
 # don't forget to update your overflow() function
 
-#    Main Author(s): 
+#    Main Author(s):
 #    Main Reviewer(s):
 
 from a1_partc import Queue
 
+
 def get_overflow_list(grid):
-	pass
+    cells = []
+    for i, lines in enumerate(grid):
+        for j, cell in enumerate(lines):
+            counter = 0
+            if i - 1 >= 0:
+                counter += 1
+            if i + 1 < len(grid):
+                counter += 1
+            if j - 1 >= 0:
+                counter += 1
+            if j + 1 < len(lines):
+                counter += 1
+            if abs(cell) >= counter:
+                cells.append([i, j])
+    if len(cells) == 0:
+        return None
+    return cells
+
 
 def overflow(grid, a_queue):
-	pass
+    overflow_list = get_overflow_list(grid)
+    if overflow_list is None:
+        return 0
+
+    # Deep Copy
+    new_grid = [[cell for cell in row] for row in grid]
+
+    print(f"Processing grid: {new_grid}")
+    print(f"Overflow list: {overflow_list}")
+
+    for cell in overflow_list:
+        i = cell[0]
+        j = cell[1]
+        if i - 1 >= 0:
+            new_grid[i - 1][j] = (new_grid[i][j] / abs(new_grid[i][j])) * (
+                abs(new_grid[i - 1][j]) + 1
+            )
+        if i + 1 < len(grid):
+            new_grid[i + 1][j] = (new_grid[i][j] / abs(new_grid[i][j])) * (
+                abs(new_grid[i + 1][j]) + 1
+            )
+        if j - 1 >= 0:
+            new_grid[i][j - 1] = (new_grid[i][j] / abs(new_grid[i][j])) * (
+                abs(new_grid[i][j - 1]) + 1
+            )
+        if j + 1 < len(grid[i]):
+            new_grid[i][j + 1] = (new_grid[i][j] / abs(new_grid[i][j])) * (
+                abs(new_grid[i][j + 1]) + 1
+            )
+
+        if new_grid[i][j] > 0:
+            new_grid[i][j] = new_grid[i][j] - abs(grid[i][j])
+        else:
+            new_grid[i][j] = -(abs(new_grid[i][j]) - abs(grid[i][j]))
+
+    a_queue.enqueue(new_grid)
+
+    return 1 + overflow(new_grid, a_queue)
