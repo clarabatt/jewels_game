136af03254db6742b0fd6ad239ff008beb6dd5b3
Clara Battesini <clarabattesini@gmail.com>
2023-12-05 08:22:52 -0500

    GameTree constructor and build_tree method

--- a3_partb.py
+++ a3_partb.py
@@ -27,6 +27,26 @@ class GameTree:
     def __init__(self, board, player, tree_height=4):
         self.player = player
         self.board = copy_board(board)
+        self.tree_height = tree_height
+        self.root = self.Node(self.board, 0, self.player, self.tree_height)
+        self.build_tree(self.root, 0)
+
+    def build_tree(self, node, current_height):
+        if current_height == self.tree_height:
+            return
+        for i in range(len(node.board)):
+            for j in range(len(node.board[i])):
+                if node.board[i][j] == 0:
+                    new_board = copy_board(node.board)
+                    new_board[i][j] = node.player
+                    new_node = self.Node(
+                        new_board,
+                        current_height + 1,
+                        node.player * -1,
+                        self.tree_height,
+                    )
+                    node.children.append(new_node)
+                    self.build_tree(new_node, current_height + 1)
 
     def get_move(self):
         height = len(self.board)
