6b5173275471a31347959807374be2d20e696b04
Clara Battesini <clarabattesini@gmail.com>
2023-12-05 07:58:02 -0500

    GameTree > Node constructor

--- a3_partb.py
+++ a3_partb.py
@@ -5,20 +5,26 @@
 
 from a3_parta import evaluate_board
 
+
 def copy_board(board):
-        current_board = []
-        height = len(board)
-        for i in range(height):
-            current_board.append(board[i].copy())
-        return current_board
+    current_board = []
+    height = len(board)
+    for i in range(height):
+        current_board.append(board[i].copy())
+    return current_board
 
 
 class GameTree:
     class Node:
-        def __init__(self, board, depth, player, tree_height = 4):
-            pass
+        def __init__(self, board, depth, player, tree_height=4):
+            self.board = board
+            self.depth = depth
+            self.player = player
+            self.tree_height = tree_height
+            self.children = []
+            self.score = None
 
-    def __init__(self, board, player, tree_height = 4):
+    def __init__(self, board, player, tree_height=4):
         self.player = player
         self.board = copy_board(board)
 
@@ -28,9 +34,7 @@ class GameTree:
         if self.player == 1:
             return (0, 0)
         else:
-            return (height-1, width-1)
-   
-    def clear_tree(self):
-        pass     
+            return (height - 1, width - 1)
 
-    
+    def clear_tree(self):
+        pass
