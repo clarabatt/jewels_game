1c74c2da8a1b8d71466b4030812ad473cc19d3f0
DanielC <127776136+ODaniel94@users.noreply.github.com>
2023-12-10 13:26:08 -0500

    Update a3_parta.py
    Updating evaluate function

--- a3_parta.py
+++ a3_parta.py
@@ -1,25 +1,19 @@
 # Main Author: Izma Farooqui
 # Main Reviewer:
 
-def overflow(board):
-    same_sign = all(all(cell >= 0 for cell in row) or all(cell < 0 for cell in row) for row in board)
-    if same_sign:
-        return 1
-    return 0
+def _check_if_both_has_same_signal(num1, num2):
+        if num1 > 0 and num2 > 0:
+            return True
+        if num1 < 0 and num2 < 0:
+            return True
+        return False
 
-
+# Part A: Evaluation Function
 def evaluate_board(board, player):
-    if check_winner(board, player):
-        return float('inf') 
-    elif check_winner(board, -player):
-        return float('-inf')  
-    else:
-        player_pieces = sum(row.count(player) for row in board)
-        opponent_pieces = sum(row.count(-player) for row in board)
-        return player_pieces - opponent_pieces
-
-def check_winner(board, player):
-    player_pieces = sum(row.count(player) for row in board)
-    opponent_pieces = sum(row.count(-player) for row in board)
-
-    return player_pieces > opponent_pieces
+    acc = 0
+    for i, lines in enumerate(board):
+        for j, cell in enumerate(lines):
+            if _check_if_both_has_same_signal(player,board[i][j]):
+                acc = acc + 1
+    return acc
+    
