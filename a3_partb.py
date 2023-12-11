a575b3c18f80a1246b2338d62928a42b96942f40
Clara Battesini <clarabattesini@gmail.com>
2023-12-11 17:17:43 -0500

    maybe finished part b

--- a3_partb.py
+++ a3_partb.py
@@ -39,12 +39,24 @@ class GameTree:
             self.children = []
             self.score = None
 
+        def is_game_over(self):
+            """
+            Returns True if the game is over, False otherwise
+            """
+            if evaluate_board(self.board, self.player) == len(self.board) * len(
+                self.board
+            ):
+                return True
+            else:
+                return False
+
     def __init__(self, board, player, tree_height=4):
         """
         Tree constructor, creates the root node and builds the tree
         """
         self.player = player
         self.board = copy_board(board)
+        self.choice = None
         self.tree_height = tree_height
         self.root = self.Node(self.board, 0, self.player, self.tree_height)
         self.build_tree(self.root, 0)
@@ -54,99 +66,89 @@ class GameTree:
         Builds the tree recursively creating child nodes for each possible state resulting from the overflow mechanic and alternates between players at each level of the tree.
         Each leaf node is assigned a score based on the evaluate_board function.
         """
+        # print(" --- Building the tree --- ")
+        # print(" current depth: ", current_depth)
+
         if current_depth == 0:
             depth_player = node.player
         else:
             depth_player = node.player * -1
 
-        print(depth_player, ' is playing')
+        # print(depth_player, " is playing")
         new_depth = current_depth + 1
-        max_score = len(node.board) * len(node.board)            
+        max_score = len(node.board) * len(node.board)
 
         if current_depth >= self.tree_height:
-            node.score = evaluate_board(node.board, self.player)
+            node.score = evaluate_board(node.board, self.player) * depth_player
             return
 
         # Generate all valid moves
-
-        # [ 0 , 2,  -2, 0, 0,  0],
-        # [ 0,  0 , -3,  -1,  0,  0],
-        # [ 0,  0,  0,  0,  0, 0],
-        # [ 0,  0,  0,  0,  2, 0],
-        # [ 0,  0,  0,  2,  0, 0]s
-
-        # [ 0 , 2,  2, 0, 0,  0],
-        # [ 0,  0 , 3,  1,  0,  0],
-        # [ 0,  0,  0,  0,  0, 0],
-        # [ 0,  0,  0,  0,  2, 0],
-        # [ 0,  0,  0,  2,  0, 0]
 
         for i in range(len(node.board)):
             for j, cell in enumerate(node.board[i]):
                 if cell == 0 or check_if_both_has_same_signal(cell, depth_player):
                     new_board = copy_board(node.board)
-                    print(new_board, 'board that is being used to play')
 
-                    # print("new_board", new_board, depth_player)
                     new_board[i][j] = new_board[i][j] + depth_player
-                    # print("new_board", new_board)
-
-                    print(new_board, 'board after player ', depth_player, ' plays')
 
                     is_overflow_possible = get_overflow_list(new_board)
-                    # print("get_overflow_list", is_overflow_possible)
-                    
 
                     # Perform the overflow and repopulate the new_board
-                    if (is_overflow_possible):
+                    if is_overflow_possible:
                         overflow_result = Queue()
                         overflow(new_board, overflow_result)
-                        while(overflow_result.size > 1):
+                        while overflow_result.size > 1:
                             dequed_element = 0
                             dequed_element = overflow_result.dequeue()
                             print(dequed_element)
                         new_board = overflow_result.dequeue()
-                        print(new_board, 'board after overflowing (if overflowing)')
-
 
                     new_node = self.Node(
                         new_board, new_depth, depth_player, node.tree_height - 1
                     )
+                    new_node.score = evaluate_board(new_board, self.player)
                     new_node.move_coordinates = (i, j)
                     node.children.append(new_node)
-                    print('new depth: ', new_depth)
-                    if new_depth < self.tree_height -1:
+                    if new_depth < self.tree_height - 1:
                         if not evaluate_board(new_board, self.player) == max_score:
                             self.build_tree(new_node, new_depth)
 
-        # if node.children:
-        #     if node.player == self.player:
-        #         node.score = max(child.score for child in node.children)
-        #     else:
-        #         node.score = min(child.score for child in node.children)
+    def minimax(self, node, depth, is_maximizing_player):
+        print(" --- Minimax --- ")
+        print(" depth: ", depth)
+        print(" board: ", node.board)
+        print(" is_maximizing_player: ", is_maximizing_player)
+        print(" node player: ", node.player)
 
-    def get_scores_by_depth(self, target_depth):
-        if target_depth < 0:
-            raise ValueError("Depth must be non-negative")
+        if node.is_game_over():
+            return node
 
-        queue = Queue()
-        queue.enqueue((self.root, 0))
-        scores_at_depth = []
+        depth += 1
+        possible_moves = []
 
-        while not queue.is_empty():
-            node, depth = queue.dequeue()
+        for childen_node in node.children:
+            possible_moves.append(childen_node)
+            # possible_moves.append(
+            #     self.minimax(childen_node, depth, not is_maximizing_player)
+            # )
 
-            if depth == target_depth:
-                scores_at_depth.append(node.score)
-            elif depth < target_depth:
-                for child in node.children:
-                    queue.enqueue((child, depth + 1))
+        if not is_maximizing_player:
+            # if the player is maximizing, return the maximum score
+            possible_moves.sort(key=lambda node: node.score, reverse=True)
+            print("MAX SCORES: ", list(map(lambda node: node.score, possible_moves)))
+            self.choice = possible_moves[0].move_coordinates
+            return possible_moves[0]
 
-        scores_at_depth.sort(reverse=True)
-        return scores_at_depth
+        else:
+            # if the player is minimizing, return the minimum score
+            possible_moves.sort(key=lambda node: node.score)
+            print("MIN SCORES: ", list(map(lambda node: node.score, possible_moves)))
+            self.choice = possible_moves[0].move_coordinates
+            return possible_moves[0]
 
     def get_move(self):
-        return self.get_scores_by_depth(self.tree_height - 1)[0].move_coordinates
+        self.minimax(self.root, 0, False)
+        return self.choice
 
     def _clear_tree_recursive(self, node):
         """
